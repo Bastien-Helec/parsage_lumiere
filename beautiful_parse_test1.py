995160f6d5564484e5d6666aaca096cf9b19598e
Bastien Helec <96698768+Bastien-Helec@users.noreply.github.com>
2022-11-23 08:08:32 +0100

    initialisation

---
+++ beautiful_parse_test1.py
@@ -0,0 +1,16 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "hhttp://www.perdu.com"
+html = urlopen(url).read()
+soup = BeautifulSoup(html, features="html.parser")
+
+for script in soup(["script", "style"]):
+    script.extract()  
+text = soup.get_text()
+
+lines = (line.strip() for line in text.splitlines())
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text)
